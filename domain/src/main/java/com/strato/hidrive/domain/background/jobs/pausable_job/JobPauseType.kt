783c2ab6f5172c1bd9d6f30dcee693aa0f74d77b
Alex Kucherenko <a.kucherenko@viseven.com>
2024-05-27 22:23:32 +0300

    TELKOMAUST-286 [cleanup] Adding Scanbot module to NextCloud [AOS] TELKOMAUST-353, TELKOMAUST-354, TELKOMAUST-355

--- domain/src/main/java/com/strato/hidrive/domain/background/jobs/pausable_job/JobPauseType.kt
+++
@@ -1,5 +0,0 @@
-package com.strato.hidrive.domain.background.jobs.pausable_job
-
-enum class JobPauseType {
-    NONE, MANUAL, OTHER
-}
--- domain/src/main/java/com/strato/hidrive/domain/background/jobs/pausable_job/PausableJob.kt
+++
@@ -1,8 +0,0 @@
-package com.strato.hidrive.domain.background.jobs.pausable_job
-
-interface PausableJob {
-    val resumeType: ResumeType
-    var pauseType: JobPauseType
-
-    fun pause(type: JobPauseType)
-}
