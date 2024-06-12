bc90c3dd42dd8a87cbd775bebbcb7c1a24215bee
Victor Mahera <v.makhera@viseven.com>
2024-06-12 19:15:10 +0300

    TELKOMAUST-366 Fix loading of images using Glide [AOS]

--- app/src/main/java/com/ionos/scanbot/image_loading/ImageLoaderImpl.kt
+++ app/src/main/java/com/ionos/scanbot/image_loading/ImageLoaderImpl.kt
@@ -1,8 +1,6 @@
 package com.ionos.scanbot.image_loading
 
-import android.widget.ImageView
 import com.ionos.scanbot.image_loader.ImageLoader
-import com.ionos.scanbot.image_loader.ImageLoaderOptions
 import com.ionos.scanbot.image_loader.ImageRequestBuilder
 import java.io.File
 import javax.inject.Inject
@@ -11,16 +9,7 @@ class ImageLoaderImpl @Inject constructor(
 ) : ImageLoader {
 
     override fun load(file: File): ImageRequestBuilder {
-        return ImageRequestBuilderImpl()
-    }
-
-    class ImageRequestBuilderImpl: ImageRequestBuilder{
-        override fun options(options: ImageLoaderOptions): ImageRequestBuilder {
-           return this
-        }
-
-        override fun into(target: ImageView) {
-        }
+        return ImageRequestBuilderImpl(file)
     }
 
 }
