e6c6ec5e61383523df42663cd5472639e70cb511
Victor Mahera <v.makhera@viseven.com>
2024-07-08 15:29:29 +0300

    TELKOMAUST-375 [remarks] Fix Scanbot camera

--- app/src/main/java/com/ionos/scanbot/utils/GetLocalFreeSpaceImpl.kt
+++ app/src/main/java/com/ionos/scanbot/utils/GetLocalFreeSpaceImpl.kt
@@ -1,16 +1,8 @@
-/*
- * Nextcloud - Android Client
- *
- * SPDX-FileCopyrightText: 2024 Your Name <your@email.com>
- * SPDX-License-Identifier: AGPL-3.0-or-later
- */
-
 package com.ionos.scanbot.utils
 
 import com.ionos.scanbot.util.GetLocalFreeSpace
 import com.owncloud.android.utils.FileStorageUtils
 import io.reactivex.Single
-import java.util.concurrent.Callable
 import javax.inject.Inject
 
 class GetLocalFreeSpaceImpl @Inject constructor() : GetLocalFreeSpace {
