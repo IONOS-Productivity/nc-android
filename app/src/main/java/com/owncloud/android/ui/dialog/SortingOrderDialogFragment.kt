e50a7987988275d7d3f8e251fcfbab008f421cf3
Denis Botvin <d.botvin@viseven.com>
2024-09-05 14:30:15 +0300

    TELKOMAUST-516 [remarks] Sorting items dialog. UI

--- app/src/main/java/com/owncloud/android/ui/dialog/SortingOrderDialogFragment.kt
+++ app/src/main/java/com/owncloud/android/ui/dialog/SortingOrderDialogFragment.kt
@@ -14,6 +14,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.DialogFragment
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.ionos.annotation.IonosCustomization
 import com.nextcloud.client.di.Injectable
 import com.owncloud.android.R
 import com.owncloud.android.databinding.SortingOrderFragmentBinding
@@ -49,6 +50,7 @@ class SortingOrderDialogFragment : DialogFragment(), Injectable {
      *
      * @param binding the parent binding
      */
+    @IonosCustomization("Remove material buttons styling")
     private fun setupDialogElements(binding: SortingOrderFragmentBinding) {
         val bindings = listOf(
             binding.sortByNameAscending to FileSortOrder.SORT_A_TO_Z,
@@ -63,15 +65,9 @@ class SortingOrderDialogFragment : DialogFragment(), Injectable {
             view.tag = sortOrder
             view.let {
                 it.setOnClickListener(OnSortOrderClickListener())
-                // Start: Disable material button styling in IONOS
-                // viewThemeUtils?.material?.colorMaterialButtonPrimaryBorderless(it)
-                // End: Disable material button styling in IONOS
             }
         }
 
-        // Start: Disable material button styling in IONOS
-        // viewThemeUtils?.material?.colorMaterialButtonPrimaryTonal(binding.cancel)
-        // End: Disable material button styling in IONOS
         binding.cancel.setOnClickListener { dismiss() }
     }
 
