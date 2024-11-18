6dcbf446bcc4f1f793c39ef4cb30e6cc1865d648
Victor Mahera <v.makhera@viseven.com>
2024-11-18 22:07:35 +0200

    TELKOMAUST-779 [-blobs] Scanbot code cleanup [AOS]

--- scanbot/src/main/java/com/ionos/scanbot/provider/SupportedLanguagesProvider.kt
+++ scanbot/src/main/java/com/ionos/scanbot/provider/SupportedLanguagesProvider.kt
@@ -1,39 +1,14 @@
 package com.ionos.scanbot.provider
 
-import android.content.Context
-import android.content.res.Configuration
-import android.os.Build
 import io.scanbot.sdk.entity.Language
-import java.util.Locale
 import javax.inject.Inject
 
 internal class SupportedLanguagesProvider @Inject constructor(
-	private val context: Context,
 ) {
 
-	fun get(): Set<Language> = mutableSetOf<Language>().apply {
-		add(Language.DEU)
-		add(Language.ENG)
-		getLocalLanguage()?.let { add(it) }
-	}
+    fun get(): Set<Language> = setOf(
+        Language.FRA,
+        Language.ENG,
+    )
 
-	private fun getLocalLanguage(): Language? = when (getLocalLanguageCode()) {
-		"fr" -> Language.FRA
-		"nl" -> Language.NLD
-		"pt" -> Language.POR
-		"es" -> Language.SPA
-		"tr" -> Language.TUR
-		else -> null
-	}
-
-	private fun getLocalLanguageCode(): String = context
-		.resources
-		.configuration
-		.getPrimaryLocale()
-		.language
-
-	private fun Configuration.getPrimaryLocale(): Locale = when {
-		Build.VERSION.SDK_INT >= Build.VERSION_CODES.N -> locales[0]
-		else -> locale
-	}
 }
