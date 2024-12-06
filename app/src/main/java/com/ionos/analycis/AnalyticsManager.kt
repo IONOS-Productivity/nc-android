93b1c5d08b38280205318f5aeced6e0ba009cf09
Victor Mahera <v.makhera@viseven.com>
2024-12-06 16:40:21 +0200

    version preparation [copyright]

--- app/src/main/java/com/ionos/analycis/AnalyticsManager.kt
+++ app/src/main/java/com/ionos/analycis/AnalyticsManager.kt
@@ -1,3 +1,10 @@
+/*
+ * IONOS HiDrive Next - Android Client
+ *
+ * SPDX-FileCopyrightText: 2024 STRATO AG.
+ * SPDX-License-Identifier: GPL-2.0
+ */
+
 package com.ionos.analycis
 
 interface AnalyticsManager {
