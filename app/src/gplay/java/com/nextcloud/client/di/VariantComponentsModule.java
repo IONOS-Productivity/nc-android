927aaceec1c876dfe55e0264fe5cd9ffe458399e
Denis Botvin <d.botvin@viseven.com>
2025-03-03 16:43:01 +0200

    TELKOMAUST-891 [revert gplay classes] Refactoring [reduce changes, simplification]

--- app/src/gplay/java/com/nextcloud/client/di/VariantComponentsModule.java
+++ app/src/gplay/java/com/nextcloud/client/di/VariantComponentsModule.java
@@ -2,13 +2,21 @@
  * Nextcloud - Android Client
  *
  * SPDX-FileCopyrightText: 2019 Chris Narkiewicz <hello@ezaquarii.com>
- * SPDX-FileCopyrightText: 2025 STRATO GmbH.
- * SPDX-License-Identifier: AGPL-3.0-or-later
+ * SPDX-License-Identifier: AGPL-3.0-or-later OR GPL-2.0-only
  */
 package com.nextcloud.client.di;
 
+import com.owncloud.android.authentication.ModifiedAuthenticatorActivity;
+import com.owncloud.android.services.firebase.NCFirebaseMessagingService;
+
 import dagger.Module;
+import dagger.android.ContributesAndroidInjector;
 
 @Module
 abstract class VariantComponentsModule {
+    @ContributesAndroidInjector
+    abstract NCFirebaseMessagingService nCFirebaseMessagingService();
+
+    @ContributesAndroidInjector
+    abstract ModifiedAuthenticatorActivity modifiedAuthenticatorActivity();
 }
--- app/src/gplay/java/com/owncloud/android/utils/SecurityUtils.java
+++ app/src/gplay/java/com/owncloud/android/utils/SecurityUtils.java
@@ -2,10 +2,27 @@
  * Nextcloud - Android Client
  *
  * SPDX-FileCopyrightText: 2018 Mario Danic <mario@lovelyhq.com>
- * SPDX-FileCopyrightText: 2025 STRATO GmbH.
- * SPDX-License-Identifier: AGPL-3.0-or-later
+ * SPDX-License-Identifier: AGPL-3.0-or-later OR GPL-2.0-only
  */
 package com.owncloud.android.utils;
 
-public class SecurityUtils {
+import android.content.Intent;
+
+import com.google.android.gms.security.ProviderInstaller;
+import com.owncloud.android.MainApp;
+
+public class SecurityUtils implements ProviderInstaller.ProviderInstallListener {
+    public SecurityUtils() {
+        ProviderInstaller.installIfNeededAsync(MainApp.getAppContext(), this);
+    }
+
+    @Override
+    public void onProviderInstalled() {
+        // Does nothing
+    }
+
+    @Override
+    public void onProviderInstallFailed(int i, Intent intent) {
+        // Does nothing
+    }
 }
