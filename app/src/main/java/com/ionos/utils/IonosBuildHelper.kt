3a7828cd118f724cd753746b137a3d2aacaf019f
Denis Botvin <d.botvin@viseven.com>
2025-03-04 20:40:36 +0200

    TELKOMAUST-919 [user icons] Alignment of icons on different platforms [AOS]

---
+++ app/src/main/java/com/ionos/utils/IonosBuildHelper.kt
@@ -0,0 +1,18 @@
+/*
+ * IONOS HiDrive Next - Android Client
+ *
+ * SPDX-FileCopyrightText: 2025 STRATO AG.
+ * SPDX-License-Identifier: GPL-2.0
+ */
+
+package com.ionos.utils
+
+import com.owncloud.android.BuildConfig
+
+object IonosBuildHelper {
+
+    @JvmStatic
+    fun isIonosFlavor(): Boolean {
+        return BuildConfig.FLAVOR == "ionos"
+    }
+}
