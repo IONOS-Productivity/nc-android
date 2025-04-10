6cffcdcfb0cc688c0e94be22aea932af7efced9a
Denis Botvin <d.botvin@viseven.com>
2025-04-10 20:15:27 +0300

    TELKOMAUST-954 [NextCloud issue] The thumbnails blink on the Media screen.

--- app/src/main/java/com/owncloud/android/ui/adapter/OCFileListDelegate.kt
+++ app/src/main/java/com/owncloud/android/ui/adapter/OCFileListDelegate.kt
@@ -94,11 +94,12 @@ class OCFileListDelegate(
         checkedFiles.clear()
     }
 
+    @IonosCustomization("Nullable galleryRowHolder")
     fun bindGalleryRowThumbnail(
         shimmer: LoaderImageView?,
         imageView: ImageView,
         file: OCFile,
-        galleryRowHolder: GalleryRowHolder,
+        galleryRowHolder: GalleryRowHolder?,
         width: Int
     ) {
         // thumbnail
@@ -113,16 +114,17 @@ class OCFileListDelegate(
 
         imageView.setOnClickListener {
             ocFileListFragmentInterface.onItemClicked(file)
-            GalleryFragment.setLastMediaItemPosition(galleryRowHolder.absoluteAdapterPosition)
+            GalleryFragment.setLastMediaItemPosition(galleryRowHolder?.absoluteAdapterPosition)
         }
     }
 
     @Suppress("ComplexMethod")
+    @IonosCustomization("Nullable galleryRowHolder")
     private fun setGalleryImage(
         file: OCFile,
         thumbnailView: ImageView,
         shimmerThumbnail: LoaderImageView?,
-        galleryRowHolder: GalleryRowHolder,
+        galleryRowHolder: GalleryRowHolder?,
         width: Int
     ) {
         // cancel previous generation, if view is re-used
@@ -169,13 +171,13 @@ class OCFileListDelegate(
                 }
                 task.setListener(object : GalleryListener {
                     override fun onSuccess() {
-                        galleryRowHolder.binding.rowLayout.invalidate()
+                        galleryRowHolder?.binding?.rowLayout?.invalidate()
                         Log_OC.d("Shimmer", "stop Shimmer")
                         DisplayUtils.stopShimmer(shimmerThumbnail, thumbnailView)
                     }
 
                     override fun onNewGalleryImage() {
-                        galleryRowHolder.redraw()
+                        galleryRowHolder?.redraw()
                     }
 
                     override fun onError() {
