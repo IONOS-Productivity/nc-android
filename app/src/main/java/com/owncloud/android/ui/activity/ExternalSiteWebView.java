abe48f6b0f5459cc9a5cb924a905e87385c53221
Alex Kucherenko <a.kucherenko@viseven.com>
2025-04-22 12:18:35 +0300

    Revert "remove v1"
    This reverts commit 9356ba3d652d61c7c1f01816cc81e596f7ef68ad.

--- app/src/main/java/com/owncloud/android/ui/activity/ExternalSiteWebView.java
+++ app/src/main/java/com/owncloud/android/ui/activity/ExternalSiteWebView.java
@@ -151,7 +151,7 @@ public class ExternalSiteWebView extends FileActivity {
             }
         });
 
-        new WebViewUtil().setProxyKKPlus(getWebView());
+        new WebViewUtil(getApplicationContext()).setProxyKKPlus(getWebView());
         getWebView().loadUrl(url);
     }
 
