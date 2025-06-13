6c621b8db11c7e1d93eaf0d928113dafa840295a
Denis Botvin <d.botvin@viseven.com>
2025-06-13 17:56:34 +0300

    TELKOMAUST-317 [convert audio fragment to kotlin] Display metadata in notification and player view [AOS]

---
+++ app/src/main/java/com/ionos/player/ui/audio/AudioPlayerSourceFragment.kt
@@ -0,0 +1,51 @@
+package com.ionos.player.ui.audio
+
+import android.os.Bundle
+import android.text.format.DateFormat
+import android.text.format.Formatter
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
+import com.ionos.player.model.PlaybackFile
+import com.owncloud.android.R
+import com.owncloud.android.databinding.PlayerAudioSourceFragmentBinding
+import dagger.android.support.AndroidSupportInjection
+import java.util.Date
+
+class AudioPlayerSourceFragment : Fragment() {
+
+    companion object {
+        private const val ARGUMENT_FILE = "ARGUMENT_FILE"
+
+        @JvmStatic
+        fun createInstance(file: PlaybackFile): Fragment {
+            val fragment = AudioPlayerSourceFragment()
+            fragment.arguments = bundleOf(ARGUMENT_FILE to file)
+            return fragment
+        }
+    }
+
+    private lateinit var file: PlaybackFile
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        AndroidSupportInjection.inject(this)
+        file = arguments?.getSerializable(ARGUMENT_FILE) as PlaybackFile
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val binding = PlayerAudioSourceFragmentBinding.inflate(inflater, container, false)
+        binding.title.text = file.getNameWithoutExtension()
+        binding.fileDetails.text = file.getDetailsText()
+        return binding.getRoot()
+    }
+
+    private fun PlaybackFile.getDetailsText(): String {
+        fun formatDate(timestamp: Long) = DateFormat.getDateFormat(context).format(Date(timestamp))
+        val size = if (contentLength > 0) Formatter.formatFileSize(context, contentLength) else ""
+        val date = if (lastModified > 0) getString(R.string.player_last_change_date, formatDate(lastModified)) else ""
+        return if (size.isNotEmpty() && date.isNotEmpty()) "$size, $date" else size + date
+    }
+}
