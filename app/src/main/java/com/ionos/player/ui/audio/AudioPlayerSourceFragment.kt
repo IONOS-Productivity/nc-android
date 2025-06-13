7d542b0171b88f20cffc3d3c7184a26810345719
Denis Botvin <d.botvin@viseven.com>
2025-06-13 17:57:34 +0300

    TELKOMAUST-317 [player audio title and artist] Display metadata in notification and player view [AOS]

--- app/src/main/java/com/ionos/player/ui/audio/AudioPlayerSourceFragment.kt
+++ app/src/main/java/com/ionos/player/ui/audio/AudioPlayerSourceFragment.kt
@@ -9,10 +9,14 @@ import android.view.ViewGroup
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import com.ionos.player.model.PlaybackFile
+import com.ionos.player.model.PlaybackModel
+import com.ionos.player.model.state.PlaybackItemState
+import com.ionos.player.model.state.PlaybackState
 import com.owncloud.android.R
 import com.owncloud.android.databinding.PlayerAudioSourceFragmentBinding
 import dagger.android.support.AndroidSupportInjection
 import java.util.Date
+import javax.inject.Inject
 
 class AudioPlayerSourceFragment : Fragment() {
 
@@ -27,6 +31,10 @@ class AudioPlayerSourceFragment : Fragment() {
         }
     }
 
+    @Inject
+    lateinit var playbackModel: PlaybackModel
+
+    private lateinit var binding: PlayerAudioSourceFragmentBinding
     private lateinit var file: PlaybackFile
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,10 +44,20 @@ class AudioPlayerSourceFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val binding = PlayerAudioSourceFragmentBinding.inflate(inflater, container, false)
+        binding = PlayerAudioSourceFragmentBinding.inflate(inflater, container, false)
         binding.title.text = file.getNameWithoutExtension()
         binding.fileDetails.text = file.getDetailsText()
         return binding.getRoot()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        playbackModel.addListener(playbackModelListener)
+    }
+
+    override fun onStop() {
+        playbackModel.removeListener(playbackModelListener)
+        super.onStop()
     }
 
     private fun PlaybackFile.getDetailsText(): String {
@@ -47,5 +65,19 @@ class AudioPlayerSourceFragment : Fragment() {
         val size = if (contentLength > 0) Formatter.formatFileSize(context, contentLength) else ""
         val date = if (lastModified > 0) getString(R.string.player_last_change_date, formatDate(lastModified)) else ""
         return if (size.isNotEmpty() && date.isNotEmpty()) "$size, $date" else size + date
+    }
+
+    private val playbackModelListener = object : PlaybackModel.Listener {
+        override fun onUpdate(state: PlaybackState) {
+            state.currentItemState.map(PlaybackItemState::metadata).ifPresent { metadata ->
+                binding.title.text = metadata.title
+                binding.artist.text = metadata.artist
+                binding.artist.visibility = if (metadata.artist.isNullOrEmpty()) View.GONE else View.VISIBLE
+            }
+        }
+
+        override fun onError(error: Throwable) {}
+
+        override fun onFilesChanged(originalFiles: List<PlaybackFile>, currentFiles: List<PlaybackFile>) {}
     }
 }
