0827638895205c6a6fbbb030b70b9052a548d79c
Denis Botvin <d.botvin@viseven.com>
2025-06-19 11:41:44 +0300

    TELKOMAUST-1020 [Copyright fix] Move latest player updates to HiDrive Next [AOS]

--- app/src/main/java/com/ionos/player/util/ScreenUtils.kt
+++ app/src/main/java/com/ionos/player/util/ScreenUtils.kt
@@ -1,3 +1,10 @@
+/*
+ * IONOS HiDrive Next - Android Client
+ *
+ * SPDX-FileCopyrightText: 2025 STRATO GmbH.
+ * SPDX-License-Identifier: GPL-2.0
+ */
+
 @file:JvmName("ScreenUtils")
 
 package com.ionos.player.util
--- app/src/main/java/com/ionos/player/util/TimeSkippableActionExecutor.java
+++ app/src/main/java/com/ionos/player/util/TimeSkippableActionExecutor.java
@@ -1,3 +1,10 @@
+/*
+ * IONOS HiDrive Next - Android Client
+ *
+ * SPDX-FileCopyrightText: 2025 STRATO GmbH.
+ * SPDX-License-Identifier: GPL-2.0
+ */
+
 package com.ionos.player.util;
 
 public class TimeSkippableActionExecutor {
