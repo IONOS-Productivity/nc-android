5afa3017083de35b1d7b55292db4d03f288f6059
Alper Öztürk <67455295+alperozturk96@users.noreply.github.com>
2025-08-04 09:53:40 +0200

    Merge pull request #15292 from nextcloud/backport/14989/stable-3.32
    [stable-3.32] Do not fetch shares during folder refresh
    # Conflicts:
    #	app/src/main/java/com/owncloud/android/operations/RefreshFolderOperation.java
    #	app/src/main/java/com/owncloud/android/ui/fragment/FileDetailSharingFragment.java
    #	app/src/main/java/com/owncloud/android/ui/fragment/FileDetailsSharingProcessFragment.kt
    #	app/src/main/java/com/owncloud/android/ui/fragment/OCFileListFragment.java
    #	app/src/main/java/com/owncloud/android/utils/DisplayUtils.java
    #	gradle/verification-metadata.xml

--- app/src/main/java/com/owncloud/android/ui/fragment/OCFileListFragment.java
+++ app/src/main/java/com/owncloud/android/ui/fragment/OCFileListFragment.java
@@ -284,6 +284,10 @@ public class OCFileListFragment extends ExtendedListFragment implements
             handleSearchEvent(searchEvent);
         }
 
+        if (getActivity() instanceof FileDisplayActivity fda) {
+            fda.startSyncFolderOperation(getCurrentFile(), true);
+        }
+
         super.onResume();
     }
 
